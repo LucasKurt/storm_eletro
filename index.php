9f512ff36582a41d454967a41075c99928aa24c9
LucasKurt <lucascorte@outlook.com>
2020-11-25 23:01:38 -0300

    mudança para StormEletro

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <link rel="stylesheet" href="./arquivos externos/CSS/estilo.css" type="text/css">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="shortcut icon" sizes="57x57" href="./img/favicon.ico">
         <title>Full Stack Eletro</title>
     </head>
     <body>   
